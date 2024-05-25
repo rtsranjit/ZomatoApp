b3764d5f79ecb5bba931d15bdd8a284946a1b66f
Ranjit Singh <ranjit.singh@aheadsofttech.com>
2024-05-25 12:35:12 +0530

    Initial Commit

---
+++ ZomatoAppS/ZomatoAppSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZomatoAppSApp.swift
+//  ZomatoAppS
+//
+//  Created by APPLE AHEAD on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ZomatoAppSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
